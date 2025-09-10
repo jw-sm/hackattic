8194c5b02dd0f8db7633bff9ce9d71a5675ea68f
del3500 <wendellsmesia@gmail.com>
2025-09-10 14:47:05 +0800

    finished unpacking

--- unpack/unpack.py
+++ unpack/unpack.py
@@ -18,35 +18,34 @@ def get_bytes():
         data = response.json()
         return data
 
-
+# get the bytes from json
 b = get_bytes().get("bytes", "")
+# decode to base64
 base64_decoded = base64.b64decode(b)
-print(f"{len(base64_decoded)} bytes")
-unpacked = struct.unpack("<8I", base64_decoded)
-print(unpacked)
-signed_int, = struct.unpack("<i", base64_decoded[0:4])
-unsigned_int, = struct.unpack("<I", base64_decoded[4:8])
-signed_short, = struct.unpack("<h", base64_decoded[8:10])
-signed_float, = struct.unpack("<f", base64_decoded[10:14])
-signed_double, = struct.unpack("<d", base64_decoded[14:22])
-another_double, = struct.unpack("!d", base64_decoded[22:30])
+
+s_int, u_int, s_short, s_float, s_double = struct.unpack("<iIhxxfd", base64_decoded[:24])
+
+network_double, = struct.unpack("!d", base64_decoded[24:32])
 
 def post_bytes():
     URL = "https://hackattic.com/challenges/help_me_unpack/solve?access_token=b5a60cc0b43768a6"
     headers = {"Content-Type": "application/json"}
+    params = {"playground": 1}
 
     payload = {
-        "int": signed_int,
-        "uint": unsigned_int,
-        "short": signed_short,
-        "float": signed_float,
-        "double": signed_double,
-        "big_endian_double": another_double,
+        "int": s_int,
+        "uint": u_int,
+        "short": s_short,
+        "float": s_float,
+        "double": s_double,
+        "big_endian_double": network_double,
     }
-    response = requests.post(URL, json=payload, headers=headers)
+    response = requests.post(URL, params=params, json=payload, headers=headers)
     print(response.status_code)
-    return response.text
+    return response.json()
 
-print(post_bytes())
+result = post_bytes()
+print(result)
+
 
 
